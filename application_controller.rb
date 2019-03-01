a7529529da51f6ec4042d11d3ffd6f423d867636
Victoria Feng <victoriaf6656@hstat.org>
2019-03-01 17:49:10 +0000

    new repo

---
+++ application_controller.rb
@@ -0,0 +1,16 @@
+require 'dotenv/load'
+require 'bundler'
+Bundler.require
+
+require_relative 'models/model.rb'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+  post '/result' do
+    erb :result
+    puts 'Result!'
+  end
+end
---
+++ models/model.rb
@@ -0,0 +1,23 @@
+#require
+
+class Video
+   def initialize(video_url)
+       @video_url = video_url
+   end
+   #Embed a video url
+   def get_video()
+       
+   end
+   #record videos using webcam
+   def record_video()
+      
+   end
+   #zoom in/out function in the video
+   def zoom_in_out()
+      
+   end
+   #Use API and get subtitles, according to video
+   def get_subtitles()
+       
+   end
+end
